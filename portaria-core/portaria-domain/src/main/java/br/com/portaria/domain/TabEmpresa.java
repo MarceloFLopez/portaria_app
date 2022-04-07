173f4d0707925de3672c9f75005ce596e438d7e5
MarceloFLopez <marceloferreiralopez@gmail.com>
2022-04-07 14:02:28 -0300

    Teste Persistence OK

--- portaria-core/portaria-domain/src/main/java/br/com/portaria/domain/TabEmpresa.java
+++ portaria-core/portaria-domain/src/main/java/br/com/portaria/domain/TabEmpresa.java
@@ -19,33 +19,38 @@ public class TabEmpresa implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String cpf;
+	private String cnpj;
 	private String name;
-
+	
+	public TabEmpresa() {
+		
+	}
+	public TabEmpresa(String cnpj, String name) {
+		this.cnpj = cnpj;
+		this.name = name;
+	}
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	public String getCpf() {
-		return cpf;
+	public String getCnpj() {
+		return cnpj;
 	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	@Override
+	public String toString() {
+		return "TabEmpresa [id=" + id + ", cnpj=" + cnpj + ", name=" + name + "]";
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -53,7 +58,6 @@ public class TabEmpresa implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -71,9 +75,5 @@ public class TabEmpresa implements Serializable {
 		return true;
 	}
 
-	@Override
-	public String toString() {
-		return "TabEmpresa [id=" + id + ", cpf=" + cpf + ", name=" + name + "]";
-	}
 
 }
--- portaria-core/portaria-persistence/src/test/java/br/com/portaria/persistence/WTestaConexao.java
+++ portaria-core/portaria-persistence/src/test/java/br/com/portaria/persistence/WTestaConexao.java
@@ -1,6 +1,6 @@
 package br.com.portaria.persistence;
 
-import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.persistence.EntityManager;
@@ -20,50 +20,61 @@ public class WTestaConexao {
 		CRUDUtil<TabEmpresa> utile = new CRUDUtil<TabEmpresa>(em, TabEmpresa.class);
 //		CRUDUtil<Transporte> utilt = new CRUDUtil<Transporte>(em, Transporte.class);
 //		CRUDUtil<Operacao> utilo = new CRUDUtil<Operacao>(em, Operacao.class);
-		
+
+		// Adicionar/Salvar
+		TabPessoa p = new TabPessoa();
+//		p.setCpf("33024955879");
+//		p.setName("Marcelo");
+//		p.setTabEmpresa(new TabEmpresa());
+//		utilp.adiciona(p);
 
 		/// recupero uma lista do banco de dados
-//		List<TabPessoa> list = utilp.listaTodos();
-//		TabPessoa p = new TabPessoa();
-//		for (TabPessoa pessoa : list) {
-//			System.out.println(pessoa);
-//		}
-		
-		//BUSCAR
+		List<TabPessoa> list = utilp.listaTodos();
+		list.forEach(System.out::println);
+
+		// BUSCAR
 //		System.out.println("Insira o ID para buscar: ");
 //		Long id = sc.nextLong();
-//		 p = em.find(TabPessoa.class, id);
+//		 p = utilp.buscaPorId(id);
 //		if (list.contains(p)) {
 //			System.out.println(p);
 //		} else {
 //			System.out.println("Id Inexistente");
 //		}
 
-		//Remove
+		// Remove
 //		System.out.println("Insira o ID para remover: ");
 //		Long id1 = sc.nextLong();
-//		p = em.find(Pessoa.class, id1);
+//		p = utilp.buscaPorId(id1);
+//		if(list.contains(p)) {
 //		utilp.remove(p);
-//		if (list.contains(p)) {
-//			System.out.println(p.getId() + " " + p.getNome());
-//		} else {
-//			System.out.println("Id não encontrado!");
+//		System.out.println("Removido com sucesso!");
+//		}else {
+//			System.out.println("Registro não existe ou já foi removido!");
 //		}
-//		sc.close();
-		
-		//Atualizar		
+
+		// Atualizar
 //		System.out.println("Insira o ID para atualizar: ");
 //		Long id1 = sc.nextLong();
-//		p = utilp.findId(id1);
-//		p.setCpf("987654321");
-//		p.setNome("Teste UpDate");
-//		p.setTelefone("11987183157");
-//		utilp.atualiza(p);
+//		p = utilp.buscaPorId(id1);
 //		System.out.println(p);
-		
-//		utilp.listaTodos();
+//		TabPessoa p2 = new TabPessoa();
+//		p2.setName("Marcelo Ferreira Lopez");
+//		p2.setCpf("00000000000");
+//		p2.setTabEmpresa(new TabEmpresa("xxxxxxxxxxx","Empresa Teste"));
+//		
+//		if (list.contains(p)) {	
+//			p.setName(p2.getName());
+//			p.setCpf(p2.getCpf());
+//			p.setTabEmpresa(p2.getTabEmpresa());
+//			utilp.atualiza(p);
+//			System.out.println("Atualizado com sucesso");
+//		} else {
+//			utilp.adiciona(p2);
+//			System.out.println("Adicionado com sucesso");
+//		}
 
-		
+//		utilp.listaTodos();
 
 //		o.setTransporte(t);		
 //		utilo.adiciona(o);
