704da33a93c0758d01e9a28b0faf972cb15bdf65
MarceloFLopez <marceloferreiralopez@gmail.com>
2022-04-07 12:31:32 -0300

    Pacote MAIN

---
+++ portaria-core/portaria-domain/src/main/java/br/com/portaria/domain/TabTransporte.java
@@ -0,0 +1,71 @@
+package br.com.portaria.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({ @NamedQuery(name = "TabTransporte.listAll", query = "SELECT t FROM TabTransporte t"),
+				@NamedQuery(name = "TabTransporte.name", query = "SELECT t FROM TabTransporte t WHERE t.placa = :placa") })
+public class TabTransporte implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String modelo;
+	private String placa;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TabTransporte other = (TabTransporte) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "TabTransporte [id=" + id + ", modelo=" + modelo + ", placa=" + placa + "]";
+	}
+
+	
+}
---
+++ portaria-core/portaria-domain/src/main/java/br/com/portaria/domain/WTESTE.java
@@ -0,0 +1,5 @@
+package br.com.portaria.domain;
+
+public class WTESTE {
+
+}
