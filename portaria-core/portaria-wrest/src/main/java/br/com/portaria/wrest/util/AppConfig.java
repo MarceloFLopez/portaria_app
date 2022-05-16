a3fce06c1b905f5665a1f3c7521eaacc2373d88d
MarceloFLopez <marceloferreiralopez@gmail.com>
2022-05-16 18:35:17 -0300

    1

--- portaria-core/portaria-wrest/src/main/java/br/com/portaria/wrest/util/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package br.com.portaria.wrest.util;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class AppConfig extends Application {
-
-}
